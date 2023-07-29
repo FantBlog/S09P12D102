4e1839fe777a5d15eb4da76638fc4e84808d8f21
박성욱 <tjddnr0110@naver.com>
2023-07-29 14:04:36 +0900

    init project

---
+++ src/main/java/com/regalaxy/phonesin/donation/model/repository/DonationRepository.java
@@ -0,0 +1,10 @@
+package com.regalaxy.phonesin.donation.model.repository;
+
+import com.regalaxy.phonesin.donation.model.entity.Donation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DonationRepository extends JpaRepository<Donation, Long> {
+    List<Donation> findTop5ByOrderById();
+}
---
+++ src/main/java/com/regalaxy/phonesin/rental/model/repository/RentalRepositoryCustom.java
@@ -0,0 +1,16 @@
+package com.regalaxy.phonesin.rental.model.repository;
+
+import com.regalaxy.phonesin.member.model.SearchDto;
+import com.regalaxy.phonesin.rental.model.ApplyDto;
+import com.regalaxy.phonesin.rental.model.RentalDetailDto;
+import com.regalaxy.phonesin.rental.model.RentalDto;
+
+import java.util.List;
+
+public interface RentalRepositoryCustom {
+    List<RentalDto> search(SearchDto searchDto);
+    boolean extension(int rental_id);
+    boolean apply(ApplyDto applyDto);
+
+    RentalDetailDto detailInfo(int rental_id);
+}
