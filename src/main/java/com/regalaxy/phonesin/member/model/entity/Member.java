2299623bd0289c26eedc5798de306c6183ea43b7
박성욱 <tjddnr0110@naver.com>
2023-07-29 14:56:12 +0900

    v1.1

--- src/main/java/com/regalaxy/phonesin/member/model/entity/Member.java
+++ src/main/java/com/regalaxy/phonesin/member/model/entity/Member.java
@@ -1,28 +1,30 @@
 package com.regalaxy.phonesin.member.model.entity;
 
 import com.regalaxy.phonesin.donation.model.entity.Donation;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
-
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import static javax.persistence.CascadeType.ALL;
 
 @Entity
 @Getter
-@Setter
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
-    private Long member_id;
+    private Long memberId;
+
     private String email;
-    private String member_name;
+    private String memberName;
     private String password;
-    private String phone_number;
+    private String phoneNumber;
     private Boolean isCha;
     private Boolean isBlackList;
     private Boolean isDelete;
@@ -30,5 +32,4 @@ public class Member {
 
     @OneToMany(mappedBy = "member",cascade = ALL,orphanRemoval = true)
     private List<Donation> donationList = new ArrayList<Donation>();
-
 }
--- src/main/java/com/regalaxy/phonesin/member/model/repository/MemberRepository.java
+++ src/main/java/com/regalaxy/phonesin/member/model/repository/MemberRepository.java
@@ -2,8 +2,6 @@ package com.regalaxy.phonesin.member.model.repository;
 
 import com.regalaxy.phonesin.member.model.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
